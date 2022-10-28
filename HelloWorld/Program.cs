544a5304565a2ec271199946bc7b0b6e1c9faaad
RickVilela <henrique_vilela_f@hotmail.com>
2022-10-28 19:52:33 -0300

    Adicionar arquivos de projeto.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String hello = "Hello World!";
+            string linguagem = "C#!";
+            Console.Write($"Esse é o meu primeiro Programa em {linguagem} {hello} ");
+
+            if (5 >= 7)
+            {
+                Console.WriteLine("");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
